a31eebcadb0f7673880330d4a2a635a85947618d
Anton Titov <antontitov718@gmail.com>
2026-02-06 15:34:21 +0200

    Update admin dashboard with Buffet theme, dishes page, and menu customization

--- app/src/Controller/Admin/DashboardController.php
+++ app/src/Controller/Admin/DashboardController.php
@@ -7,13 +7,14 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
 {
     public function index(): Response
     {
-        return parent::index();
+        return $this->render('admin/buffet.html.twig');
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
@@ -36,15 +37,21 @@ class DashboardController extends AbstractDashboardController
         // return $this->render('some/path/my-dashboard.html.twig');
     }
 
+    #[Route('/admin/dishes', name: 'admin_dishes')]
+    public function dishes(): Response
+    {
+        return $this->render('admin/dishes.html.twig');
+    }
+
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('App');
+            ->setTitle('Buffet');
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToDashboard('Buffet', 'fa fa-cutlery');
+        yield MenuItem::linkToRoute('Dishes', 'fa fa-plate-wheat', 'admin_dishes');
     }
 }
